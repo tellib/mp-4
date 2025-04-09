49488ce3ed8dd5ef31a79882513064d0238d6745
Berk <berktellioglu@gmail.com>
2025-04-09 00:42:31 -0400

    Updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ export default function Footer() {
   return (
     <footer className="bg-gradient-to-l from-blue-500 to-blue-600 py-4 text-center text-sm text-white">
       <p>
-        All Rights Reserved by Berk Tellioglu{' '}
+        Created by{' '}
         <a href="https://github.com/tellib" target="_blank">
-          Credits
+          Berk Tellioglu
         </a>{' '}
         &#169;
       </p>
