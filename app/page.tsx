6970032beea861aa29d356b7b7c6efdb78480711
Berk <berktellioglu@gmail.com>
2025-04-09 00:31:41 -0400

    Added suspense around HomePage SearchCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Loading from '@/components/Loading'
 import SearchCard from '@/components/SearchCard'
+import { Suspense } from 'react'
 
 export default function HomePage() {
   return (
     <div className="mx-auto flex flex-1 items-center justify-center">
-      <SearchCard />
+      <Suspense fallback={<Loading />}>
+        <SearchCard />
+      </Suspense>
     </div>
   )
 }
